b8d9be6fe9ccb60f3cbcd28e85755023d681aba8
Douglas Rokakis <doug.rokakis@gmail.com>
2023-01-19 13:10:51 -0500

    forgotten code exercises

---
+++ src/bankaccountapp/IBaseRate.java
@@ -0,0 +1,14 @@
+package bankaccountapp;
+
+public interface IBaseRate {
+	
+	// Write a method that returns the base rate
+	default double getBaseRate() {
+		return 2.5;
+	}
+	
+	// Constructor to set base properties and initialize the account
+	
+	// List common methods
+
+}
